4a3fb7b7d74dd9d289a4b099b63a6db186e120ae
Chris Packett <chrispackett1998@gmail.com>
2020-01-08 22:00:11 -0500

    Added the ability to add questions.

--- src/pages/NewProject.vue
+++ src/pages/NewProject.vue
@@ -2,8 +2,17 @@
     <q-page class="q-pa-lg flex column hero-grey">
         <div class="row justify-between">
             <h5 class="q-ma-none text-dark text-weight-medium">{{ newProject.name }}</h5>
-            <q-btn color="secondary" label="New Question +" rounded class="gt-sm"></q-btn>
-            <q-btn color="secondary" label="+" rounded class="lt-sm"></q-btn>
+            <q-btn
+                color="secondary"
+                label="New Question +"
+                rounded class="gt-sm"
+                @click="createNewQuestion"></q-btn>
+            <q-btn
+                color="secondary"
+                label="+"
+                rounded
+                class="lt-sm"
+                @click="createNewQuestion"></q-btn>
         </div>
         <div class="row">
             <h6 class="q-ma-none text-grey text-subtitle2">{{ newProject.client }}</h6>
@@ -29,13 +38,37 @@
                         </q-item-section>
                     </q-item>
 
-                    <q-item v-for="question in questions" :key="question.id">
-                        <q-item-section top class="col-5">
+                    <q-item v-for="(question, index) in questions" :key="question.id">
+                        <q-item-section
+                            v-if="!question.isEditMode"
+                            top
+                            class="col-5">
                             <q-item-label class="q-mt-sm">{{ question.text }}</q-item-label>
                         </q-item-section>
 
-                        <q-item-section class="col-3">
+                        <q-item-section
+                            v-if="question.isEditMode"
+                            top
+                            class="col-5">
+                            <q-input
+                                v-model="question.text"
+                                class="q-pr-xl" />
+                        </q-item-section>
+
+                        <q-item-section
+                            v-if="!question.isEditMode"
+                            class="col-3">
                             {{ question.inputType }}
+                        </q-item-section>
+
+                        <q-item-section
+                            v-if="question.isEditMode"
+                            class="col-3">
+                            <q-select
+                                v-model="question.inputType"
+                                :options="inputTypes"
+                                class="q-pr-xl">
+                            </q-select>
                         </q-item-section>
 
                         <q-item-section class="col-2 gt-sm">
@@ -52,9 +85,34 @@
                             </div>
                             <q-menu anchor="bottom right" self="top right">
                                 <q-list>
-                                <q-item clickable dense class="text-primary">Edit</q-item>
-                                <q-separator />
-                                <q-item clickable dense class="text-negative">Delete</q-item>
+                                    <q-item
+                                        v-if="question.isEditMode"
+                                        @click="saveQuestion(index)"
+                                        v-close-popup
+                                        clickable
+                                        dense
+                                        class="text-secondary">
+                                        Save
+                                    </q-item>
+                                    <q-separator />
+                                    <q-item
+                                        v-if="!question.isEditMode"
+                                        @click="editQuestion(index)"
+                                        v-close-popup
+                                        clickable
+                                        dense
+                                        class="text-primary">
+                                        Edit
+                                    </q-item>
+                                    <q-separator />
+                                    <q-item
+                                        @click="deleteQuestion(index)"
+                                        v-close-popup
+                                        clickable
+                                        dense
+                                        class="text-negative">
+                                        Delete
+                                    </q-item>
                                 </q-list>
                             </q-menu>
                         </q-item-section>
@@ -77,18 +135,65 @@ export default {
         {
           id: 1,
           text: 'What is your Company\'s Name?',
-          inputType: 'Input',
+          inputType: 'Input Textfield',
           resources: 'Video',
-          comments: 'Bubble'
+          comments: 'Bubble',
+          isEditMode: false
         },
         {
           id: 2,
           text: 'What is your Mission Statement?',
-          inputType: 'Dropdown',
+          inputType: 'Select Dropdown',
           resources: 'Video',
-          comments: 'Bubble'
+          comments: 'Bubble',
+          isEditMode: false
         }
+      ],
+      inputTypes: [
+        'Input Textfield',
+        'Select Dropdown',
+        'Radio',
+        'Checkbox'
       ]
+    }
+  },
+  methods: {
+    createNewQuestion () {
+      const nextId = this.questions.length !== 0
+        ? Math.max(...this.questions.map(q => q.id)) + 1
+        : 1
+
+      const newQuestion = {
+        id: nextId,
+        text: '',
+        inputType: '',
+        resources: 'Video',
+        comments: 'Bubble',
+        isEditMode: true
+      }
+
+      this.questions.push(newQuestion)
+    },
+    saveQuestion (index) {
+      this.questions = this.questions.map((q, i) => {
+        if (i === index) {
+          q.isEditMode = false
+        }
+
+        return q
+      })
+    },
+    editQuestion (index) {
+      this.questions = this.questions.map((q, i) => {
+        if (i === index) {
+          q.isEditMode = true
+        }
+
+        return q
+      })
+    },
+    deleteQuestion (index) {
+      this.questions = this.questions.filter((q, i) => i !== index)
     }
   }
 }
